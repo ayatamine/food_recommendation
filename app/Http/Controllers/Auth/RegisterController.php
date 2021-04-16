8acd2bf4a0360864cbc0a17791eb32f4202309c8
ayatamine <ayatir04@gmail.com>
2021-04-16 02:28:21 +0100

    redirect to rating after register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/rating';
 
     /**
      * Create a new controller instance.
